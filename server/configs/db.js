f824848015d435d4a82d10256a493f45d191edcd
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-02-11 18:52:02 +0100

    User Model

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
       console.log("Database connected");
     });
-    await mongoose.connect(`${process.env.MONGODB_URL}/alfacart`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/alfacart`);
   } catch (error) {
     console.error(error.message);
   }

0328a04951205814872b30e160b73d051a870f16
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-02-16 09:05:59 +0100

    User Controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,7 +35,7 @@ export const register = async (req, res) => {
       user: { email: user.email, name: user.name },
     });
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message); 
     res.json({ success: false, message: error.message });
   }
 };

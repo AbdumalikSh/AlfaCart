1f8ff6903e101d82626478ba458455791daf83b6
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-03-05 12:39:06 +0100

    ..

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist", // This is the folder Vercel needs
   },
-  plugins: [react(), tailwindcss()],
 });
